bbb59f5c92e5c48cf5d17d21faec6f071eb0abac
Aliyan Sajid <aliyansajid127@gmail.com>
2025-12-24 16:52:20 +0500

    Initial Commit

---
+++ timely/timelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  timelyApp.swift
+//  timely
+//
+//  Created by Aliyan Sajid on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct timelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
